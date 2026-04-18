6ed5b32f626aa61ba2cf798cbb4403832f26cc95
IrtezaIshaq <150837618+irtazahup@users.noreply.github.com>
2026-04-18 22:36:03 +0500

    brain.py me system prompt change kiya

--- brain.py
+++ brain.py
@@ -17,6 +17,7 @@ def get_ai_decision(user_text, patient_name, chat_history):
 
     system_prompt = f"""
     You are 'MedCompanion Dispatcher'. Today's date is {today}.
+    
     Your only job is to decide if you need to query the database for {patient_name}.
 
     STRATEGY:
@@ -36,6 +37,63 @@ def get_ai_decision(user_text, patient_name, chat_history):
 
     RESPONSE FORMAT:
     Respond ONLY with a JSON object.
+    
+    The Table's scheme of Available Tools are:
+    create table public.adherence_logs (
+  id uuid not null default extensions.uuid_generate_v4 (),
+  profile_id uuid null,
+  reminder_id uuid null,
+  scheduled_time timestamp with time zone not null,
+  status text null default 'pending'::text,
+  responded_at timestamp with time zone null,
+  medication_id uuid null,
+  constraint adherence_logs_pkey primary key (id),
+  constraint unique_reminder_per_day unique (reminder_id, scheduled_time),
+  constraint adherence_logs_medication_id_fkey foreign KEY (medication_id) references medications (id) on delete CASCADE,
+  constraint adherence_logs_profile_id_fkey foreign KEY (profile_id) references profiles (id) on delete CASCADE,
+  constraint adherence_logs_reminder_id_fkey foreign KEY (reminder_id) references reminders (id) on delete CASCADE,
+  constraint adherence_logs_status_check check (
+    (
+      status = any (
+        array[
+          'pending'::text,
+          'taken'::text,
+          'skipped'::text,
+          'missed'::text
+        ]
+      )
+    )
+  )
+) TABLESPACE pg_default;
+create table public.medications (
+  id uuid not null default extensions.uuid_generate_v4 (),
+  profile_id uuid null,
+  med_name text not null,
+  dosage text not null,
+  start_date date not null,
+  end_date date not null,
+  created_at timestamp with time zone null default now(),
+  constraint medications_pkey primary key (id),
+  constraint medications_profile_id_fkey foreign KEY (profile_id) references profiles (id) on delete CASCADE
+) TABLESPACE pg_default;
+create table public.profiles (
+  id uuid not null default extensions.uuid_generate_v4 (),
+  attendant_phone text not null,
+  patient_phone text not null,
+  patient_name text not null,
+  timezone text not null,
+  created_at timestamp with time zone null default now(),
+  constraint profiles_pkey primary key (id)
+) TABLESPACE pg_default;
+create table public.profiles (
+  id uuid not null default extensions.uuid_generate_v4 (),
+  attendant_phone text not null,
+  patient_phone text not null,
+  patient_name text not null,
+  timezone text not null,
+  created_at timestamp with time zone null default now(),
+  constraint profiles_pkey primary key (id)
+) TABLESPACE pg_default;
     """
     try:
         completion = client.chat.completions.create(
